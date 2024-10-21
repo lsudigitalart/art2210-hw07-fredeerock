94b75de964b05e824751398233b2fdbd746a259e
Derick Ostrenko <dostrenko@lsu.edu>
2024-10-21 14:38:06 -0500

    mouse

--- sketch.js
+++ sketch.js
@@ -1,23 +1,25 @@
-let sound;
+let soundFile;
 let soundPlayed = false;
 let startTime;
 
 function preload() {
-    sound = loadSound('halloween.mp3');
+    soundFile = loadSound('halloween.mp3');
 }
 
 function setup() {
     createCanvas(400, 400);
     startTime = millis();
+    userStartAudio();
+}
+
+function mousePressed() {
+  if (!soundFile.isPlaying()) {
+    soundFile.play();
+  }
 }
 
 function draw() {
     background(220);
-
-    if (!soundPlayed) {
-        sound.play();
-        soundPlayed = true;
-    }
 
     if (millis() - startTime > 3000) {
         ellipse(width / 2, height / 2, 50);
